41d90e35972e0a731c6f0228fcce97b1272507af
Bogomolov Denis <denis.bogomolov.nsk@yandex.ru>
2018-06-28 21:23:12 +0200

    Create empty xcode project

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Kino
+//
+//  Created by Denis Bogomolov on 28/06/2018.
+//  Copyright © 2018 kino. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+}
