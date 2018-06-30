d41bfcbce8bdfe0f425eca5400b69b9150811987
Bogomolov Denis <denis.bogomolov.nsk@yandex.ru>
2018-06-30 23:50:18 +0200

    Add Configuration file

---
+++ Sources/Configuration.swift
@@ -0,0 +1,13 @@
+//
+//  Configuration.swift
+//  Kino
+//
+//  Created by Denis Bogomolov on 30/06/2018.
+//  Copyright © 2018 kino. All rights reserved.
+//
+
+import Foundation
+
+enum Configurations {
+    static let moviedbApiKey = "<#PUT HERE YOUR API KEY#>"
+}
